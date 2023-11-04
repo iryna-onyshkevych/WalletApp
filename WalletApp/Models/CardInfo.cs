047946459cb2705552be360590dc55d0cbf2d8f3
iryna-onyshkevych <onyshkevych.iryna@gmail.com>
2023-11-04 18:13:47 +0200

    CardInfo and Transaction classes added

---
+++ WalletApp/Models/CardInfo.cs
@@ -0,0 +1,44 @@
+﻿using System.Transactions;
+
+namespace WalletApp.Models
+{
+    public class CardInfo
+    {
+        public int UserId { get; set; }
+        public int CardBalance { get; set; } 
+        public int Available { get; set; }
+        public string NoPaymentDue { get; set; } 
+
+        public string DailyPoints { get; set; }
+        public List<Transaction> Transactions { get; set; }
+        //public string GetDailyPoints(int date, int month)
+        //{
+        //    return DailyPoints;
+        //}
+
+        public decimal GetAvailable()
+        {
+            return 1500 - this.CardBalance;
+        }
+
+        public int GetCardBalance()
+        {
+            Random random = new Random();
+            return random.Next(1500);
+        }
+
+        public string GetNoPaymentDue(string month)
+        {
+            return $"You’ve paid your {month} balance";
+        }
+
+        public CardInfo(int userId, string month)
+        {
+            UserId = userId;
+            //Available = GetAvailable();
+            CardBalance = GetCardBalance();
+            NoPaymentDue = GetNoPaymentDue(month);
+        }
+    }
+}
+}
---
+++ WalletApp/Models/Transaction.cs
@@ -0,0 +1,16 @@
+﻿namespace WalletApp.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; } //foreign key
+        public string Type { get; set; } //options
+        public string Sum { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Date { get; set; } //options
+        public bool Pending { get; set; } //options
+        public string? AuthorizedUser { get; set; }
+        //public ImageSource Icon { get; set; }
+    }
+}
