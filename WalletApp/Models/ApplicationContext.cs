70b7c47f188bcb3a433b362b5c51a1f59184ea88
iryna-onyshkevych <onyshkevych.iryna@gmail.com>
2023-11-04 21:15:29 +0200

    appsettings and GetData api added

---
+++ WalletApp/Models/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace WalletApp.Models
+{
+    public class ApplicationContext:DbContext
+    {
+        public DbSet<Transaction> Transactions { get; set; } = null!;
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                       .AddJsonFile("appsettings.json").Build();
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+            }
+        }
+    }
+}
